8fe8d358e5f32e9a43679f322390259b8f6c412b
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-06-09 22:46:18 -0300

    Create multer config

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import multer from './config/multer'
 
 import PointsController from './controller/PointsController'
 import ItemsController from './controller/ItemsController'
@@ -9,6 +10,6 @@ router.get('/items', ItemsController.index)
 
 router.get('/points', PointsController.index)
 router.get('/points/:id', PointsController.show)
-router.post('/points', PointsController.create)
+router.post('/points', multer.single('image'), PointsController.create)
 
 export default router
