6dc50ade25ae266e273cd4fd99f3de543555807d
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-06-23 21:34:42 -0300

    Move knex connection config to config path

--- src/configs/knex.ts
+++ src/configs/knex.ts
@@ -1,12 +1,10 @@
 import knex from 'knex'
 import path from 'path'
 
-const connection = knex({
+export default knex({
   client: 'sqlite3',
   connection: {
-    filename: path.resolve(__dirname, 'database.sqlite')
+    filename: path.resolve(__dirname, '..', 'database', 'database.sqlite')
   },
   useNullAsDefault: true
 })
-
-export default connection
