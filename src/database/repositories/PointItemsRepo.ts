e445f693009aa473f310e8a731e4bcbdb848fff6
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-06-25 21:54:53 -0300

    Rename PointItemRepo to PointItemsRepo

--- src/database/repositories/PointItemsRepo.ts
+++ src/database/repositories/PointItemsRepo.ts
@@ -3,11 +3,11 @@ import knex from '../../configs/knex'
 export default class ItemRepository {
   private table = knex('point_items')
 
-  public async create(item_id: number, point_id: number) {
+  public async create(pointId: number, itemId: number) {
     try {
       const [id] = await this.table.insert({
-        item_id,
-        point_id
+        point_id: pointId,
+        item_id: itemId
       })
 
       return id
