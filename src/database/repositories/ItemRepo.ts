1567bb86ee5867e25e24fb90f8f37b53f69e09f6
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-06-26 23:42:07 -0300

    Create method find items by point

--- src/database/repositories/ItemRepo.ts
+++ src/database/repositories/ItemRepo.ts
@@ -3,6 +3,18 @@ import knex from '../../configs/knex'
 export default class ItemRepository {
   private table = knex('items')
 
+  public async findByPoint(pointId: number) {
+    try {
+      const itemList = await this.table
+        .join('point_items', 'items.id', '=', 'point_items.item_id')
+        .where('point_items.point_id', pointId)
+
+      return itemList
+    } catch (error) {
+      throw new Error(error.message)
+    }
+  }
+
   public async findAll() {
     try {
       const itemList = await this.table.select('*')
