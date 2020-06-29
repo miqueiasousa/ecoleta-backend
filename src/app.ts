f61f5daba9962d501480ce5f05194ee0fa7cda2b
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-06-29 15:44:55 -0300

    Import point router

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import path from 'path'
 import cors from 'cors'
 import ItemRouter from './routes/ItemRouter'
+import PointRouter from './routes/PointRouter'
 import { errors } from 'celebrate'
 
 const app = express()
@@ -10,6 +11,7 @@ app.use(cors())
 app.use(express.json())
 
 app.use(ItemRouter)
+app.use(PointRouter)
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 
