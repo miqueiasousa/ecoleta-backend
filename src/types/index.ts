a08cfb6e7836aa764a8839b0d6c6f1102dbcca59
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-07-07 23:22:02 -0300

    Create Point Interface

--- src/types/index.ts
+++ src/types/index.ts
@@ -3,3 +3,15 @@ export interface IItem {
   title: string
   image_url: string
 }
+
+export interface IPoint {
+  id: number
+  name: string
+  email: string
+  whatsapp: string
+  street: string
+  number: number
+  city: string
+  uf: string
+  image_url: string
+}
