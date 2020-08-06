8853edd993b2aae2f5e69b29aae33d6086dd8933
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-08-06 16:16:12 -0300

    Create Point Interfaces

---
+++ src/interfaces/IPoint.ts
@@ -0,0 +1,18 @@
+export interface IPoint {
+  id: number
+  name: string
+  street: string
+  number: number
+  city: string
+  uf: string
+  image_url: string
+}
+
+export interface IPointDTO {
+  name: string
+  street: string
+  number: number
+  city: string
+  uf: string
+  image_url: string
+}
