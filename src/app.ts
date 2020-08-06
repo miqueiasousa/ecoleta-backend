d57635c351a45099a903225b225b5d01d078d735
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-08-05 21:43:28 -0300

    Create new route to static files

--- src/app.ts
+++ src/app.ts
@@ -10,6 +10,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
+app.use(express.static(path.resolve(__dirname, '..', 'public')))
 app.use('/uploads', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(ItemRouter)
 app.use(PointRouter)
