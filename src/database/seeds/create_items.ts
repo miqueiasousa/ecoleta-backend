9ca42986f7d90251cfd6191b8eb39124bf4c40bc
Miqueias Sousa <miqueiassousa80@gmail.com>
2020-08-05 21:44:24 -0300

    Fix image URL

--- src/database/seeds/create_items.ts
+++ src/database/seeds/create_items.ts
@@ -4,27 +4,27 @@ export function seed(knex: Knex): QueryBuilder {
   return knex('items').insert([
     {
       title: 'Lâmpadas',
-      image_url: 'http://localhost:3030/uploads/lampadas.svg'
+      image_url: 'http://localhost:3030/images/lampadas.svg'
     },
     {
       title: 'Pilhas e Baterias',
-      image_url: 'http://localhost:3030/uploads/baterias.svg'
+      image_url: 'http://localhost:3030/images/baterias.svg'
     },
     {
       title: 'Papéis e Papelão',
-      image_url: 'http://localhost:3030/uploads/papeis-papelao.svg'
+      image_url: 'http://localhost:3030/images/papeis-papelao.svg'
     },
     {
       title: 'Resíduos Eletrônicos',
-      image_url: 'http://localhost:3030/uploads/eletronicos.svg'
+      image_url: 'http://localhost:3030/images/eletronicos.svg'
     },
     {
       title: 'Resíduos Orgânicos',
-      image_url: 'http://localhost:3030/uploads/organicos.svg'
+      image_url: 'http://localhost:3030/images/organicos.svg'
     },
     {
       title: 'Óleo de Cozinha',
-      image_url: 'http://localhost:3030/uploads/oleo.svg'
+      image_url: 'http://localhost:3030/images/oleo.svg'
     }
   ])
 }
